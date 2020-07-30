d7a40755c86f57e7cb7753afdce68ac7aa1f8369
Chris Diehl <culktclassik@gmail.com>
2020-07-30 12:39:53 -0500

    Update entrypoint script

--- gomining.sh
+++ gomining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /ethminer/ethminer \
-  -P stratum://${ETH_ADDR}.${HOSTNAME}-${GPU_ID}@us1.ethermine.org:4444 \
-  -P stratum://${ETH_ADDR}.${HOSTNAME}-${GPU_ID}@us2.ethermine.org:4444 \
-  --opencl-device ${GPU_ID} \
-  -G --HWMON --report-hr
+ -G --HWMON 1 --report-hr \
+ --opencl-device ${GPU_ID} \
+ -P stratum://${ETH_ADDR}.${HOSTNAME}-${GPU_ID}@us1.ethermine.org:4444 \
+ -P stratum://${ETH_ADDR}.${HOSTNAME}-${GPU_ID}@us2.ethermine.org:4444
